6865e79360fdd92e9a957fa1cbae396ba3d94384
BJ4501 <neko.bj4501@gmail.com>
2018-02-26 17:31:54 +0800

    递归控制：链表的构建与反转链表操作

---
+++ src/com/bj/MoocInterview/CreateLinkedList.java
@@ -0,0 +1,39 @@
+package com.bj.MoocInterview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 链表创建
+ * Created by BJ on 2018/2/26.
+ */
+public class CreateLinkedList {
+
+    /**
+     * Create linked list
+     * @param data Data to create the list
+     * @return head of the linked list
+     */
+    public Node createLinkedList(List<Integer> data){
+        if (data.isEmpty()){
+            return null;
+        }
+        //第一个Node
+        Node firstNode = new Node(data.get(0));
+        //递归：subList去掉0之后的所有元素,如果没有元素subList会返回空串
+        Node headOfSublist = createLinkedList(data.subList(1,data.size()));
+        firstNode.setNext(headOfSublist);
+        return firstNode;
+    }
+
+    public static void main(String[] args) {
+        CreateLinkedList c = new CreateLinkedList();
+
+        Node.printLinkedList(c.createLinkedList(new ArrayList<>()));
+        Node.printLinkedList(c.createLinkedList(Arrays.asList(1)));
+        Node.printLinkedList(c.createLinkedList(Arrays.asList(1,2,3,4,5)));
+
+    }
+
+}
---
+++ src/com/bj/MoocInterview/ReverseLinkedList.java
@@ -0,0 +1,46 @@
+package com.bj.MoocInterview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 反转链表
+ * Created by BJ on 2018/2/26.
+ */
+public class ReverseLinkedList {
+
+    /**
+     * Reverse a linked list.
+     * 1-2-3-null revert to null-1-2-3
+     * @param head head the linked list to reverse
+     * @return head of the reversed linked list
+     */
+    public Node reverseList(Node head){
+        //size == 0
+        if (head == null){
+            return null;
+        }
+        //size == 1
+        if (head.getNext() == null){
+            return head;
+        }
+        Node newHead = reverseList(head.getNext());
+        head.getNext().setNext(head);
+        head.setNext(null);
+
+        return newHead;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        CreateLinkedList c = new CreateLinkedList();
+        ReverseLinkedList rll = new ReverseLinkedList();
+
+        Node.printLinkedList(rll.reverseList(c.createLinkedList(new ArrayList<>())));
+        Node.printLinkedList(rll.reverseList(c.createLinkedList(Arrays.asList(1))));
+        Node.printLinkedList(rll.reverseList(c.createLinkedList(Arrays.asList(1,2,3,4,5))));
+
+    }
+}
